4a036198fd70cd5e9a49346ea6440a8e3bcff352
Noardewyn <karpovichalexej@gmail.com>
2021-07-28 17:31:02 +0300

    init commit

---
+++ OpenGL_Render.cpp
@@ -0,0 +1,12 @@
+﻿// OpenGL_Render.cpp: определяет точку входа для приложения.
+//
+
+#include "OpenGL_Render.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
