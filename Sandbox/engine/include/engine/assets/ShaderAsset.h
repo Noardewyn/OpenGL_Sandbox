a8d462f3eeabf3de9839a3d911483707d139d86e
Noardewyn <karpovichalexej@gmail.com>
2021-09-12 22:28:23 +0300

    add: ShaderAsset

---
+++ Sandbox/engine/include/engine/assets/ShaderAsset.h
@@ -0,0 +1,27 @@
+#ifndef __SHADER_ASSET_H__
+#define __SHADER_ASSET_H__
+
+#include <string>
+
+#include "Render/Shader.h"
+#include "engine/assets/Asset.h"
+
+namespace engine {
+
+  class ShaderAsset : public Asset {
+  public:
+    ShaderAsset(const std::string& asset_path, bool auto_reload_enabled = false);
+    virtual ~ShaderAsset() = default;
+
+    virtual void load() override;
+    virtual void unload() override;
+    virtual void reload() override;
+
+    Renderer::Shader& getShader();
+  private:
+    Renderer::Shader _shader;
+  };
+
+} // namespace engine
+
+#endif // __SHADER_ASSET_H__
---
+++ Sandbox/engine/src/assets/ShaderAsset.cpp
@@ -0,0 +1,34 @@
+#include "engine/assets/ShaderAsset.h"
+
+#include <filesystem>
+#include "engine/assets/AssetManager.h"
+
+namespace engine {
+
+  ShaderAsset::ShaderAsset(const std::string& asset_path, bool auto_reload_enabled)
+    : Asset::Asset(asset_path, AssetType::Shader, auto_reload_enabled) {
+    
+  }
+
+  void ShaderAsset::load() {
+    std::filesystem::path full_path(AssetManager::assetsPath() + getPath());
+    full_path.replace_extension();
+
+    _shader = Renderer::Shader(full_path.string() + ".frag", full_path.string() + ".vs");
+  }
+
+  void ShaderAsset::unload() {
+
+  }
+
+  void ShaderAsset::reload() {
+    set_reloading(true);
+    load();
+    set_reloading(false);
+  }
+
+  Renderer::Shader& ShaderAsset::getShader() {
+    return _shader;
+  }
+
+} // namespace engine
